2469c8b33cc5eb61c40eca2a76f5a373c7d170c8
pfgallagher <pfgallagher@gmail.com>
2019-04-14 16:51:55 -0500

    Add useNewUrlParser option to Mongoose connection

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const Person = new Schema({
 	name: {
