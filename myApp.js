ec64c5c333ebff71098df9242f5fa997bae026d8
pfgallagher <pfgallagher@gmail.com>
2019-04-14 19:19:05 -0500

    Add findAndUpdate function

--- myApp.js
+++ myApp.js
@@ -66,25 +66,17 @@ const findEditThenSave = (personId, done) => {
 	});
 };
 
-/** 9) New Update : Use `findOneAndUpdate()` */
-
-// Recent versions of `mongoose` have methods to simplify documents updating.
-// Some more advanced features (i.e. pre/post hooks, validation) beahve
-// differently with this approach, so the 'Classic' method is still useful in
-// many situations. `findByIdAndUpdate()` can be used when searching by Id.
-//
-// Find a person by `name` and set her age to `20`. Use the function parameter
-// `personName` as search key.
-//
-// Hint: We want you to return the **updated** document. In order to do that
-// you need to pass the options document `{ new: true }` as the 3rd argument
-// to `findOneAndUpdate()`. By default the method
-// passes the unmodified object to its callback.
-
-const findAndUpdate = function(personName, done) {
+const findAndUpdate = (personName, done) => {
 	const ageToSet = 20;
-
-	done(null /*, data*/);
+	Person.findOneAndUpdate(
+		{ name: personName },
+		{ age: ageToSet },
+		{ new: true },
+		(err, data) => {
+			if (err) return done(err);
+			done(null, data);
+		},
+	);
 };
 
 /** # CRU[D] part IV - DELETE #
